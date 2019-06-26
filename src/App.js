af8addcf8c49d25d0027c6bbff3ffb2a2f7c9c3d
svcDmsAdmin <svcDmsAdmin@nb.com>
2019-06-26 13:53:15 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles/sass/app.scss";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+
+import Landing1 from "./components/Landing1";
+import Landing2 from "./components/Landing2";
+import Demo from "./components/Demo";
+import Theme from "./Theme";
+import Landing3 from "./components/Landing3";
+
+function App() {
+  return (
+    <MuiThemeProvider theme={Theme}>
+      {/* <Router basename="/react-landing"> */}
+      <Router basename="/">
+        <Switch>
+          <Route path="/" component={Demo} exact />
+          <Route path="/landing1" component={Landing1} />
+          <Route path="/landing2" component={Landing2} />
+          <Route path="/landing3" component={Landing3} />
+          {/* <Route component={Error} /> */}
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
